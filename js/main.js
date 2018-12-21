239c81f4fd417739b1a9be4ee9e4380e03712bbe
DanielPiskorz <eel5426@gmail.com>
2018-12-21 23:29:29 +0100

    Scrolling jQuery animations

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
   $(".menu-icon").on("click", function() {
         $("nav ul").toggleClass("showing");
   });
+
 });
 
 // Scrolling Effect
@@ -12,8 +13,31 @@ $(window).on("scroll", function() {
   if($(window).scrollTop()) {
         $('nav').addClass('black');
   }
-
   else {
         $('nav').removeClass('black');
+       
+  }
+
+  let scroll = $(window).scrollTop();
+
+  if(scroll > 400){
+    $('.logo').fadeIn('slow');
+  }else{
+    $('.logo').fadeOut('slow');
   }
 })
+
+// Navbar links scrolling
+
+$(function(){
+  $('li a').on('click',function(e){
+    e.preventDefault();
+
+    const navbarOffset = 120;
+    $('html, body').animate({
+      
+      scrollTop: ($($(e.target).attr("href")).offset().top - navbarOffset) < 0 ?
+        0 : ($($(e.target).attr("href")).offset().top - navbarOffset)
+    }, 1000);
+  });
+});
